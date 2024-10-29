7ab3f0a46ef685e61f368b790bc4fb3b1c93fb51
urdof7 <urdof7@gmail.com>
2024-10-29 04:37:14 -0400

    additional kick data

--- kick_data.py
+++ kick_data.py
@@ -1,3 +1,12 @@
+
+# Directions:
+# 1 - Top left
+# 2 - Middle top
+# 3 - Top right
+# 4 - Bottom left
+# 5 - Middle bottom
+# 6 - Bottom right
+
 kick_data = [
     {
         "video_name": "Argentina v France： Full Penalty Shoot-out ｜ 2022 #FIFAWorldCup Final [MCWJNOfJoSM].mp4",
@@ -31,6 +40,15 @@ kick_data = [
         "timestamp": "02:32",
         "direction": 5,
         "player_name": "Paulo Dybala",
+        "player_team": "Argentina",
+        "goal_scored": True,
+        "num_frames": 10
+    },
+    {
+        "video_name": "Argentina v France： Full Penalty Shoot-out ｜ 2022 #FIFAWorldCup Final [MCWJNOfJoSM].mp4",
+        "timestamp": "04:15",
+        "direction": 4,
+        "player_name": None,
         "player_team": "Argentina",
         "goal_scored": True,
         "num_frames": 10
